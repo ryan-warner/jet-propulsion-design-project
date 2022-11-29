039e13f7d8e33ebb57ca243c28710830d5b1240f
Ryan Warner <ryan.warner@gatech.edu>
2022-11-28 20:31:27 -0500

    Simple tests for objects

---
+++ testClasses.m
@@ -0,0 +1,17 @@
+function testClasses
+
+testDiffuser = diffuser(0.92, 1.4, 2);
+
+testDiffuser.diffuserEfficiency
+testDiffuser.ramDrag
+
+testDiffuser.temperatureChange(92)
+testDiffuser.pressureChange(429)
+
+testFan = fan(0.2, 1.2, 1.4, 0.8)
+
+testCompressor = compressor(1.2, 1.4, 0.9)
+
+testBurner = burner(1.2, 1.4, 1, 45, 0.9)
+
+end
