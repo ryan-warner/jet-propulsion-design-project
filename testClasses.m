23635e563f21f0c37d1263b0642d3224999f4494
Ryan Warner <ryan.warner@gatech.edu>
2022-11-28 22:37:19 -0500

    Began afterburner implementation

--- testClasses.m
+++ testClasses.m
@@ -5,7 +5,7 @@ testDiffuser = diffuser(0.92, 1.4, 2);
 testDiffuser.diffuserEfficiency
 testDiffuser.ramDrag
 
-testDiffuser.temperatureChange(92)
+testDiffuser = testDiffuser.temperatureChange(92)
 testDiffuser.pressureChange(429)
 
 testFan = fan(0.2, 1.2, 1.4, 0.8)
@@ -13,5 +13,6 @@ testFan = fan(0.2, 1.2, 1.4, 0.8)
 testCompressor = compressor(1.2, 1.4, 0.9)
 
 testBurner = burner(1.2, 1.4, 1, 45, 0.9)
+
 
 end
