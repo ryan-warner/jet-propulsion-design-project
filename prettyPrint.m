177e7721debd85788ce95513ae3a474bcd79bfd4
Ryan Warner <ryan.warner@gatech.edu>
2022-11-29 20:02:43 -0500

    Converted to kPa

--- prettyPrint.m
+++ prettyPrint.m
@@ -1,9 +1,9 @@
 function result = prettyPrint(stages)
     result = table();
-    result.("TAmb") = [stages{1,1}.temperatureInitial];
-    result.("PAmb") = [stages{1,1}.pressureInitial];
+    result.("TAmb") = [round(stages{1,1}.temperatureInitial,2)];
+    result.("PAmb") = [round(stages{1,1}.pressureInitial / 1000, 2)];
     for stage = stages
-        result.(sprintf("T0%s",stage{1}.station)) = [stage{1}.temperatureFinal];
-        result.(sprintf("P0%s",stage{1}.station)) = [stage{1}.pressureFinal];
+        result.(sprintf("T0%s",stage{1}.station)) = [round(stage{1}.temperatureFinal, 2)];
+        result.(sprintf("P0%s",stage{1}.station)) = [round(stage{1}.pressureFinal / 1000, 2)];
     end
 end
